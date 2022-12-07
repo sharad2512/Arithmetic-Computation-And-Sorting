3fc88039f30fca2745b2ba6e6fec02d5f13dc71b
sharad2512 <shnarute95@gmail.com>
2022-12-07 21:06:26 +0530

    [Sharad]-uc6 upload successfully

---
+++ uc6Dictionary.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -x
+declare -A Outputs
+
+Outputs[uc1]="no opration"
+Outputs[uc2]="answer1"
+Outputs[uc3]="answer2"
+Outputs[uc4]="answer3"
+Outputs[uc5]="answer4"
+
+echo "Outputs=${Outputs[@]}"
+
